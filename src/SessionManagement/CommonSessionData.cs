1abd4317316f24e9f7d13374ebee9321706106a9
Tim Hanewich <tahanewich@live.com>
2021-08-13 09:24:11 -0400

    Made session track and session type nullable

--- src/SessionManagement/CommonSessionData.cs
+++ src/SessionManagement/CommonSessionData.cs
@@ -17,8 +17,8 @@ namespace ApexVisual.SessionManagement
         public byte TrackTemperatureCelsius {get; set;}
         public byte AirTemperatureCelsius {get; set;}
         public byte LapsInRace {get; set;}
-        public SessionType ThisSessionType {get; set;}
-        public Track SessionTrack {get; set;}
+        public SessionType? ThisSessionType {get; set;}
+        public Track? SessionTrack {get; set;}
         public ushort SessionTimeLeftSeconds {get; set;}
         public ushort SessionDurationSeconds {get; set;}
         public SafetyCarStatus CurrentSafetyCarStatus {get; set;}
