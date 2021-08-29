aeb5213f02129b0daa99667ecca5af2995bd4d59
Tim Hanewich <tahanewich@live.com>
2021-08-29 10:46:01 -0400

    Added ApexVisualSettings

---
+++ src/Cloud/Settings/ApexVisualSettings.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ApexVisual.Cloud.Settings
+{
+    public class ApexVisualSettings
+    {
+        public bool UserAccountsDisabled {get; set;} //Both user login and user registration will be disabled if this is turned to true.
+    }
+}
