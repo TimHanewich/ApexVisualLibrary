23f135b0f9074ea4c0a8e3654185c3380e09cdce
Tim Hanewich <tahanewich@live.com>
2021-09-21 09:28:09 -0400

    Added OriginalCapture

---
+++ src/SessionDocumentation/OriginalCapture.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ApexVisual.SessionDocumentation
+{
+    public class OriginalCapture
+    {
+        public UInt64 SessionId {get; set;}
+        public DateTime CapturedAtUtc {get; set;}
+    }
+}
