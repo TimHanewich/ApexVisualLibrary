9d917c9951aa9cae6a3de1d7f4bd8e74cb9b9847
Tim Hanewich <tahanewich@live.com>
2021-09-22 14:03:52 -0400

    Added method for deleting telemetry snapshots from a session

--- src/Cloud/Storage/SqlExtensions.cs
+++ src/Cloud/Storage/SqlExtensions.cs
@@ -1206,6 +1206,12 @@ namespace ApexVisual.Cloud.Storage
             return ToReturn.ToArray();
         }
 
+        public static async Task DeleteTelemetrySnapshotsAsync(this ApexVisualManager avm, ulong from_session_id)
+        {
+            string cmd = "delete ts from TelemetrySnapshot as ts inner join Lap on TelemetrySnapshot.FromLap = Lap.Id where Lap.FromSession = " + ApexVisualToolkit.ULongToLong(from_session_id).ToString();
+            await ExecuteNonQueryAsync(avm, cmd);
+        }
+
         private static TelemetrySnapshot ExtractTelemetrySnapshotFromSqlDataReader(SqlDataReader dr)
         {
             TelemetrySnapshot ToReturn = new TelemetrySnapshot();
